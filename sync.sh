05c1ae621b2f7bd1bc27131a391f20e742d5f05f
SeanJA <sean.sandy@gmail.com>
2021-11-23 00:51:40 -0500

    deploy script (#1)

---
+++ sync.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Local .env
+if [ -f .env ]; then
+    # Load Environment Variables
+    export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
+fi
+
+rsync -e "/usr/bin/ssh" --verbose  --bwlimit=2000 -av ./dist/ $USER@$DOMAIN:/home/$USER/$DOMAIN/
